769d4d1863f3e017e9a3aac65d76ec905d994215
amashou <amano.criaju@gmail.com>
2021-04-09 12:08:21 +0900

    クライアントとサーバとのajax通信

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import CleanUp from './CleanUp';
+import axios from 'axios'
 
 const App: React.FC = () => {
   const [status, setStatus] = useState("first step");
+  const [display, setDisplay] = useState(true);
+
+  const getXemBalance = () => {
+    // axios.defaults.baseURL = 'http://localhost:8080';
+    // axios.defaults.headers.get['Content-Type'] = 'application/json';
+    // axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+    axios.get("http://localhost:8080/xem").then((res: any) => console.log(res.data)).catch((err) => {console.log("レスポンスに発生しました:", err)})
+  }
+
   return (
     <div className="App">
       <h4>{status}</h4>
       <header className="App-header">
+        {display &&  <CleanUp />}
+        <button onClick={() => setDisplay(!display)}>Toggle Display</button>
+
+        <button onClick={getXemBalance}>XENの情報を取得する</button>
+
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
